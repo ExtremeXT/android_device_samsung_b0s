00000c7ca9e9f22010b3e591f86a89f4b7c196d9
Tim Zimmerman <tim@linux4.de>
2022-04-22 11:30:52 +0200

    r0s: NOP SecCameraToRil::enable/disable m_sendRequest()
    * Fixes switching to different camera IDs when using OSS camera
      provider.
    
    Change-Id: If8e1350cf271c847d146e415911c81698d7d38d1
    Co-authored-by: Josip K <kelecicj@gmail.com>
    Signed-off-by: ExtremeXT <extremext@lineageos.org>

--- extract-files.py
+++ extract-files.py
@@ -28,6 +28,9 @@ blob_fixups: blob_fixups_user_type = {
         'vendor/lib64/libexynoscamera3.so',
     ): blob_fixup()
         .add_needed('libui_shim.so'),
+    'vendor/lib64/libexynoscamera3.so': blob_fixup()
+        .sig_replace('16 00 00 94 0B 00 00 14', '1F 20 03 D5 0B 00 00 14')  # NOP SecCameraIPCtoRIL::enable m_sendRequest()
+        .sig_replace('92 FF FF 97 0B 00 00 14', '1F 20 03 D5 0B 00 00 14'), # NOP SecCameraIPCtoRIL::disable m_sendRequest()
 }  # fmt: skip
 
 module = ExtractUtilsModule(
