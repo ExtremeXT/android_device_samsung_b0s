cf97ea9d7dd1d97ff0c59b03a5bbe8b0f5e6f53f
ExtremeXT <extremext@lineageos.org>
2025-11-07 16:07:25 +0200

    r0s: Shim libutils and sensorndkbridge for vdis
    Change-Id: If4179674f459d528794c9813d0a8a21bedcbb4b7
    Signed-off-by: ExtremeXT <extremext@lineageos.org>

--- extract-files.py
+++ extract-files.py
@@ -38,6 +38,10 @@ blob_fixups: blob_fixups_user_type = {
    'vendor/lib64/libeis_core.so': blob_fixup()
         .add_needed('libutils-v32.so')
         .binary_regex_replace(b'_ZN7android6Thread3runEPKcim', b'_ZN7utils326Thread3runEPKcim'),
+    'vendor/lib64/libvdis_core.so': blob_fixup()
+        .add_needed('libshim_sensorndkbridge.so')
+        .add_needed('libutils-v32.so')
+        .binary_regex_replace(b'_ZN7android6Thread3runEPKcim', b'_ZN7utils326Thread3runEPKcim'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
