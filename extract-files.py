64caa984e74dcb439b19981841204b5e1d2b7ed0
ExtremeXT <extremext@lineageos.org>
2025-11-07 16:08:00 +0200

    r0s: Shim libutils for libeis_core
    Change-Id: I5f8c4a5a0dff2f348aee14aa1f53bfe481590e8c
    Signed-off-by: ExtremeXT <extremext@lineageos.org>

--- extract-files.py
+++ extract-files.py
@@ -35,6 +35,9 @@ blob_fixups: blob_fixups_user_type = {
     'vendor/lib64/sensors.sensorhub.so': blob_fixup()
         .add_needed('libutils-v32.so')
         .binary_regex_replace(b'_ZN7android6Thread3runEPKcim', b'_ZN7utils326Thread3runEPKcim'),
+   'vendor/lib64/libeis_core.so': blob_fixup()
+        .add_needed('libutils-v32.so')
+        .binary_regex_replace(b'_ZN7android6Thread3runEPKcim', b'_ZN7utils326Thread3runEPKcim'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
