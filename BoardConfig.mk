96a58021c214ed75c3ee66a18d1696450c01767b
ExtremeXT <extremext@lineageos.org>
2025-11-08 21:23:00 +0200

    r0s: Setup basic skeleton tree
    Change-Id: Ia16449041fed40b424cffe6efbecadc6fbdfff7b
    Signed-off-by: ExtremeXT <extremext@lineageos.org>

---
+++ BoardConfig.mk
@@ -0,0 +1,9 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from Common Tree
+include device/samsung/s5e9925-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/samsung/r0s
---
+++ lineage_r0s.mk
@@ -0,0 +1,7 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from r0s device
+$(call inherit-product, device/samsung/r0s/device.mk)
