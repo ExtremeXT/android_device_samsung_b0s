9db22f563d84e003adf970bb1bc7ccfcb90515f3
ExtremeXT <extremext@lineageos.org>
2025-11-08 21:27:27 +0200

    r0s: Define device identifiers
    Change-Id: Ifa165e3391c267abc40f4f98e0dc59d5d2d5f106
    Signed-off-by: ExtremeXT <extremext@lineageos.org>

--- lineage_r0s.mk
+++ lineage_r0s.mk
@@ -5,3 +5,15 @@
 
 # Inherit from r0s device
 $(call inherit-product, device/samsung/r0s/device.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := r0s
+PRODUCT_NAME := lineage_r0s
+PRODUCT_MODEL := SM-S901B
+
+# Use the latest approved GMS identifiers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    DeviceProduct=r0sxxx \
+    SystemName=r0sxxx \
+    BuildFingerprint=samsung/r0sxxx/r0s:12/SP1A.210812.016/S901BXXSNGZD7:user/release-keys \
+    BuildDesc="r0sxxx-user 12 SP1A.210812.016 S901BXXSNGZD7 release-keys"
