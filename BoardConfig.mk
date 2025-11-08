bbf53103356737fc5e05e34f0be00ff8e78fe781
ExtremeXT <extremext@lineageos.org>
2025-11-08 21:46:57 +0200

    r0s: Define display properties
    Change-Id: If88a8f6af3140f02d627d5977b732701db566c43
    Signed-off-by: ExtremeXT <extremext@lineageos.org>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,3 +7,8 @@
 include device/samsung/s5e9925-common/BoardConfigCommon.mk
 
 DEVICE_PATH := device/samsung/r0s
+
+# Display
+TARGET_SCREEN_DENSITY := 480
+TARGET_SCREEN_HEIGHT := 2340
+TARGET_SCREEN_WIDTH := 1080
